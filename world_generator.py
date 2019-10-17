a676c09ebaf985e6ce1e900e9fb84d6358b427b4
kmakiela <makiela11@gmail.com>
2019-10-17 11:52:49 +0200

    logic makes sense

--- world_generator.py
+++ world_generator.py
@@ -18,73 +18,81 @@ class City:
 
 
 class World:
-    def __init__(self, cities_no, size, height_max, value_max):
+    def __init__(self, cities_no, size):
         self.cities_no = cities_no
         self.size = size
-        self.height_mesh = self.create_height_mesh(height_max)
-        self.value_mesh = self.create_value_mesh(value_max)
+        self.height_mesh = self.create_height_mesh()
+        self.value_mesh = self.create_value_mesh()
         self.cities = self.populate_world()
         print(self.cities)
 
-        l = lambda x, y: 1 if (x, y) in [(c.x, c.y) for c in self.cities] else 0
-        plot(size, np.vectorize(l))
+        #  l = lambda x, y: 1 if (x, y) in [(c.x, c.y) for c in self.cities] else 0
+        #  plot(size, np.vectorize(l))
 
-        self.price_matrix, self.value_matrix = self.create_roads_matrics()
-
-        print("Price: ", self.price_matrix,"Value: ", self.value_matrix)
+        self.road_matrix, self.cost_matrix, self.value_matrix = self.create_road_matrices()
+        print("No of roads: \n", np.count_nonzero(self.road_matrix))
+        print("Roads: \n", self.road_matrix)
+        print("Cost: \n", self.cost_matrix)
+        print("Value: \n", self.value_matrix)
 
         plt.show()
 
+    def create_height_mesh(self):
+        return self.create_somewhat_random_normalized_mesh()
 
-    def create_height_mesh(self, height_max):
+    def create_value_mesh(self):
+        return self.create_somewhat_random_normalized_mesh()
+
+    def create_somewhat_random_normalized_mesh(self):
         x = np.arange(0, self.size)
         y = np.arange(0, self.size)
         X, Y = np.meshgrid(x, y)
-        # TODO placeholder height function
-        Z = (X + Y)
+        Z = (X + Y) * np.random.rand(self.size, self.size)
         max_z = np.amax(Z)
-        Z = Z*height_max/max_z
+        Z = Z/max_z
         return Z
-
-    def create_value_mesh(self, value_max):
-        return self.create_height_mesh(value_max)
 
     def populate_world(self):
         n = self.cities_no
         size = self.size
         coords = random.sample([(x, y) for x in range(size) for y in range(size)], n)
-        return [City(x, y, self.height_mesh[x,y], self.value_mesh[x,y]) for (x,y) in coords]
+        return [City(x, y, self.height_mesh[x, y], self.value_mesh[x, y]) for (x, y) in coords]
 
-    def create_roads_matrics(self):
+    def create_road_matrices(self):
         n = self.cities_no
         cities = self.cities
-        price_matrix = np.ones([n, n])
+        cost_matrix = np.zeros([n, n])
         value_matrix = np.zeros([n, n])
-
-        # d= [maybe_create_road(c1,c2)  for c1 in self.cities for c2 in self.cities if c1!=c2]
+        road_matrix = np.zeros([n, n])
 
         for x1, c1 in enumerate(cities):
             for y1, c2 in enumerate(cities):
                 if c1 != c2:
-                    x=x1-1
-                    y=y1-1
-                    # implement logic for choosing when to put a road
-                    if random.random() > 0.5:
-                        price_matrix[x][y] = self.get_price(c1, c2)
-                        value_matrix[x][y] = self.get_value(c1, c2)
-        return price_matrix, value_matrix
+                    x = x1-1
+                    y = y1-1
 
-    def get_price(self, c1, c2):
-        dist = ((c1.x-c2.x)**2+(c1.y-c2.y)**2)**1.0/2
-        price = dist*(c2.z-c1.z)
-        if price <= 0:
-            return dist*0.01
+                    dist_to_centre = ((c1.x - self.size/2) ** 2 + (c1.y - self.size/2) ** 2) ** (1.0 / 2)
+                    radius = (dist_to_centre / 2) + (0.35 * self.size)  # the range of a city is ~40% of a total space
+                    # print(radius, dist_to_centre, c1.x, c1.y)
+                    city_dist = ((c1.x - c2.x) ** 2 + (c1.y - c2.y) ** 2) ** (1.0 / 2)
+
+                    if city_dist < radius:
+                        road_matrix[x][y] = 1
+                        cost_matrix[x][y] = self.get_cost(c1, c2)
+                        value_matrix[x][y] = self.get_value(c1, c2)
+        return road_matrix, cost_matrix, value_matrix
+
+    def get_cost(self, c1, c2):
+        dist = ((c1.x-c2.x)**2+(c1.y-c2.y)**2)**(1.0/2)
+        cost = dist*(c2.z-c1.z) * random.uniform(0.7, 1.0)
+        if cost < 0:
+            return cost * (-0.6)
         else:
-            return price
+            return cost
 
     def get_value(self, c1, c2):
         dist = ((c1.x-c2.x)**2+(c1.y-c2.y)**2)**1.0/2
-        value = dist*abs(c2.value-c1.value)
+        value = dist*(c2.value + c1.value) * random.uniform(0.5, 1.0)
         return value
 
 
@@ -106,24 +114,7 @@ def plot(size, z):
 
 def main(cities_no=10, world_size=10, world_height=100):
 
-    w = World(10, 10, 10, 10)
-
-    # x = np.arange(0, world_size, 0.1)
-    # y = np.arange(0, world_size, 0.1)
-    # X, Y = np.meshgrid(x, y)
-    # Z = X + Y
-    # print(Z)
-    # for x in range(13):
-    #     for y in range(13):
-    #         Z[x][y] = 13
-    #
-    # surf = ax.plot_surface(X, Y, Z,
-    #                    linewidth=0, antialiased=False)
-    #
-    # # h = plt.contourf(x, y, Z)
-    # plt.show()
-    #
-    # print(random.random())
+    w = World(10, 10)
 
 
 if __name__=='__main__':
